848e2ae78bdb75e2649d452345ad932a9148280a
Jou-ching Sung <georgesung@gmail.com>
2016-01-16 00:16:47 -0500

    Completed enemy movement and randomization, and player movement

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, v) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -8,8 +8,12 @@ var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
 
     // Enemy's location
-    this.x = 4*tile_w;
-    this.y = 1*tile_h - 20;
+    this.model_row_offset = -20;  // pixel offset to align model to tile
+    this.x = x*tile_w;
+    this.y = y*tile_h + this.model_row_offset;
+
+    // Enemy's velocity (i.e. speed), in pixels per second
+    this.v = v;
 };
 
 // Update the enemy's position, required method for game
@@ -18,6 +22,19 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // v = dx/dt; v = velocity, dx = pixel displacement, dt = delta time in seconds
+    // dx = v * dt
+    var dx = this.v * dt;
+    this.x = this.x + dx;
+
+    // If the enemy goes off screen to the right, loop back to beginning position
+    // Also generate a new random row location, and new random velocity
+    if (this.x > document.getElementsByTagName('canvas')[0].width) {
+        this.x = -tile_w;
+        this.y = getRandomIntInclusive(1,3)*tile_h + this.model_row_offset;
+        this.v = getRandomIntInclusive(min_v, max_v);
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -30,8 +47,10 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 var Player = function() {
     this.sprite = 'images/char-boy.png';
+
+    this.model_row_offset = -10;
     this.x = 2*tile_w;
-    this.y = 4*tile_h - 10;
+    this.y = 4*tile_h + this.model_row_offset;
 };
 
 Player.prototype.update = function(dt) {
@@ -42,19 +61,48 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function() {
-
+Player.prototype.handleInput = function(key) {
+    // Move the player in the intended direction, if possible
+    // Make sure the player never moves outside the playing area
+    if (key === 'left' && this.x > 0) {
+        this.x -= tile_w;
+    }
+    else if (key === 'up' && this.y - this.model_row_offset > 0) {
+        this.y -= tile_h;
+    }
+    else if (key === 'right' && this.x + tile_w < document.getElementsByTagName('canvas')[0].width) {
+        this.x += tile_w;
+    }
+    else if (key === 'down' && this.y - this.model_row_offset < 5*tile_h) {  // FIXME: Just know there are 6 rows, not flexible code
+        this.y += tile_h;
+    }
 };
+
+// Random integer generation helper function, taken from:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 // x y locations will be multiplied by this amount
 // to get the enemies and player aligned to tiles
 tile_w = 101;
 tile_h = 83;
 
+// min/max values for random enemy velocities
+min_v = 100;
+max_v = 400;
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-allEnemies = [new Enemy()];
+allEnemies = [
+    new Enemy(0, 1, getRandomIntInclusive(min_v, max_v)),
+    new Enemy(0, 2, getRandomIntInclusive(min_v, max_v)),
+    new Enemy(0, 3, getRandomIntInclusive(min_v, max_v))
+];
 player = new Player();
 
 // This listens for key presses and sends the keys to your
